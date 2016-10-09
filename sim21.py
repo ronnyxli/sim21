596c3a10d29f9c87655bde643b06465377249c15
Ronny Li <rli@proteus.com>
2016-10-09 01:43:27 -0700

    Update 2

--- sim21.py
+++ sim21.py
@@ -7,64 +7,41 @@ Created on Sun Jul 31 19:57:40 2016
 
 import numpy
 import itertools
-import argparse
 import random
 import pandas as pd
 
 import pdb
 
 
-# ARGUMENTS: 
-# NUM_DECKS
-
-def parse_args():
-    # add arguments to parser
-    parser = argparse.ArgumentParser(description = 'Simulation parameters')    
-    parser.add_argument('-d', metavar='NumDecks', type=int, dest='num_decks',
-        default=1, help='Number of card decks to use (default = 1, max = 8)') 
-    parser.add_argument('-p', metavar='NumPlayers', type=int, dest='num_players',
-        default=1, help='Number of players at the table (default = 1, max = 5)') 
-    parser.add_argument('-c', metavar='NumCredits', type=int, dest='num_credits',
-        default=500, help='Number of card decks to use (default = 500, max = 2500)') 
-    # create dictionary of arguments 
-    arg_dict = {}
-    # check number of decks
-    if (parser.parse_args().num_decks <= 8) & (parser.parse_args().num_decks >= 1):
-        arg_dict['NUM_DECKS'] = parser.parse_args().num_decks
-    else:
-        print('Number of decks must be between 1 and 8... using default value of 1')
-        arg_dict['NUM_DECKS'] = 1
-    # check number of players
-    if (parser.parse_args().num_players <= 5) & (parser.parse_args().num_players >= 1):
-        arg_dict['NUM_PLAYERS'] = parser.parse_args().num_players
-    else:
-        print('Number of players must be between 1 and 5... using default value of 1')
-        arg_dict['NUM_PLAYERS'] = 1
-    # check number of credits
-    if (parser.parse_args().num_credits <= 2500) & (parser.parse_args().num_credits >= 100):
-        arg_dict['NUM_CREDITS'] = parser.parse_args().num_credits
-    else:
-        print('Cash must be between 100 and 2500... using default value of 500')
-        arg_dict['NUM_CREDITS'] = 500
-    return arg_dict
-        
-
-def new_deck(N):
+def init_game(num_decks, player_name, player_cash, num_players):
     """
-    Function to aggregate N decks into one shuffled deck
+    Function to initialize the game by creating a shuffled deck and data frame for all players
+        Args: # of decks, name of current player, total # of players
+        Returns: DECK (list of cards in random order), PLAYERS (pandas data frame)
     """
+    # aggregate all decks into one shuffled deck
     DECK = []
-    DECK.append(['J']*(4*N))
-    DECK.append(['Q']*(4*N))
-    DECK.append(['K']*(4*N))
-    for n in range(1,11):
-        DECK.append([n]*(4*N))
+    DECK.append(['J']*(4*num_decks))
+    DECK.append(['Q']*(4*num_decks))
+    DECK.append(['K']*(4*num_decks))
+    DECK.append(['A']*(4*num_decks))
+    for n in range(2,11):
+        DECK.append([n]*(4*num_decks))
     DECK = list(itertools.chain.from_iterable(DECK))
     random.shuffle(DECK)
-    return DECK
+    
+    index = ['Dealer', player_name]
+    for p in range(0,num_players-1):
+        index.append('Player ' + str(p+2))
+    columns = ['Card 1', 'Card 2', 'Score', 'Cash']
+    PLAYERS = pd.DataFrame(index=index, columns=columns)
+    PLAYERS.loc["Dealer" , "Cash"] = 'Infinite'
+    PLAYERS.loc[1:, "Cash"] = player_cash
+    
+    return DECK, PLAYERS
+    
 
-
-def query_bet():
+def query_bet(min_bet):
     """
     Queries user for the amount of $ to bet
     """
@@ -75,8 +52,8 @@ def query_bet():
             bet = int(bet)
             if (bet > player_cash):
                 print('Bet cannot exceed $' + str(player_cash))
-            elif (bet < 1):
-                print('Bet must be at last $1')
+            elif (bet < min_bet):
+                print('Minimum bet is ' + str(min_bet))
             else:
                 valid_bet = 1
         except ValueError:
@@ -84,80 +61,99 @@ def query_bet():
     return bet
 
 
-def compute_prob(CARDS):
+def calc_score(row):
+    cards_ind = ['Card 1', 'Card 2']
+    score = 0
+    for c in cards_ind:
+        if (row[c] is 'J'):
+            score = score + 10
+        elif (row[c] is 'Q'):
+            score = score + 10
+        elif (row[c] is 'K'):
+            score = score + 10
+        elif (row[c] is 'A'):
+            score = score + 10
+        else:
+            score = score + row[c]      
+    return score
+
+
+def deal(DECK, PLAYERS):
+    
+    ind = PLAYERS.index.tolist()
+
+    # deal first card to each player (dealer last?)
+    for p in range(0,len(ind)):
+        PLAYERS.set_value(ind[p], 'Card 1', DECK[p])
+        
+    # deal second card to each player (dealer hidden)
+    for p in range(0,len(ind)):
+        if ind[p] is "Dealer":
+            dealer_last_card = DECK[p]
+        else:
+            PLAYERS.set_value(ind[p], 'Card 2', DECK[p])    
+            # compute score
+            score = calc_score(PLAYERS.loc[ind[p],:])
+            PLAYERS.set_value(ind[p], 'Score', score)  
+    
+    return PLAYERS, dealer_last_card
+
+
+
+
+def compute_prob(DECK):
     # probability dictionary
     return 0
-
-
-def deal(deck, cards, numPlayers):
-    state = []
-    
-    # deal one card to each player first
-    for p in range(0,numPlayers):
-        deck[p]
-    
-    # one card to dealer
-    
-    # second card to each player
-    
-    # second card to dealer
-    
-    
-    return deck, cards, state
-
 
 
 
 
 if __name__ == "__main__":
     
-    # parse arguments
-    # arg_dict = parse_args()   
-    
     # prompt user for info
     # player_name = input('Enter your name: ')
-    player_names = 'Ronny'
-    num_players = 5
-    num_decks = 8
+    player_name = 'Ronny'
+    num_players = 3
+    num_decks = 1
     player_cash = 2500
     
-    # initialize deck
-    DECK = new_deck(num_decks)
+    # initialize game
+    DECK, PLAYERS = init_game(num_decks, player_name, player_cash, num_players)
     
-    # initialize pandas data frame for players
-    
-    PLAYERS = pd.DataFrame()
-
-    # start game
+    # START GAME
     play = 1
 
     while play:
         
         # query player for bet
-        bet = query_bet()
-  
-        # deal cards
+        bet = query_bet(15)
         
-        pdb.set_trace()         
+        PLAYERS, dealer_last_card = deal(DECK, PLAYERS)
         
-        action = input('Type H to hit, S to stand, Q to quit: ')
+        print(PLAYERS.to_string())     
+        
+        query_action = 1
+        while query_action:
+        
+            action = input('Type H to hit, St to stand, Sp to split, D to double, Q to quit: ')
     
-        if action == 'H':
-            break
-        elif action == 'S':
-            break
-        elif action == 'Q':
-            play = 0
-        else:
-            break
+            if action == 'H':
+                query_action = 0
+                pdb.set_trace()
+                # add extra column
+            elif action == 'St':
+                query_action = 0
+                pdb.set_trace()
+            elif action == 'Sp':
+                query_action = 0
+                pdb.set_trace()
+            elif action == 'D':
+                query_action = 0
+                pdb.set_trace()
+            elif action == 'Q':
+                query_action = 0
+                play = 0
+            else:
+                print('Invalid choice')
     
     
-    
-    
-    arg_dict['NUM_PLAYERS']
-    arg_dict['NUM_CREDITS']
-    
-    
-    
-
-
